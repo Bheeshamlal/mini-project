843658f18fc71d8bd8216dfd853488ee62ee72d5
Bheeshamlal <bheeshamlal.bscssef20@iba-suk.edu.pk>
2024-11-05 16:33:25 +0500

    mini project

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-let btn = document.querySelector("button");
-btn.addEventListener("click", function(){
-    console.log("haha");
+let div = document.querySelector("div");
+let ul = document.querySelector("ul");
+let lis = document.querySelectorAll("li");
+
+
+div.addEventListener("click", function(){
+    console.log("div clicked");
 });
+
+ul.addEventListener("click", function(){
+    console.log("ul clicked");
+});
+
+for(li of lis){
+li.addEventListener("click", function(){
+    console.log("li clicked");
+});
+}
+
+
+
+
+
